4af2af724daa9587cb48d35d094b57dbc4530434
root <root@vps-a45a22a1.vps.ovh.net>
2023-05-24 07:50:54 +0000

    fixato php, continua ad esserre presente il problema di connessione

--- connection.php
+++ connection.php
@@ -15,12 +15,14 @@ if (!$conn->connect_error) {
 $sql="SELECT * FROM studenti";
 $result=$conn->query($sql);
 
-if(&result->num_rows>0){
+if($result->num_rows>0){
 	//output data of each row
 	while($row=$result->fetch_assoc()){
 		echo "id: " . $row["id_studente"]. " - Nome: " . $row["nome"]. "- Cognome: " . $row["cognome"]. "- Voto: " . $row["voto"]. "<br>";
-	} else {
-		echo "0 results";
+	}
+}
+else{
+echo "0 results";
 }
 $conn->close();
 ?>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <!--
+    Modified from the Debian original for Ubuntu
+    Last updated: 2022-03-22
+    See: https://launchpad.net/bugs/1966004
+  -->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Apache2 Ubuntu Default Page: It works</title>  </head>
+  <body>
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+include "connection.php"
+?>
+  </body>
+</html>
